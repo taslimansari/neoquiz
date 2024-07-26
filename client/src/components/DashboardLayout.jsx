b37849a1add2d82ca4596951cfad3374d4ce6e0e
AbhishekNavgan95 <navganabhishek90@gmail.com>
2024-07-26 15:08:33 +0530

    added styles to dashboard nav

--- client/src/components/DashboardLayout.jsx
+++ client/src/components/DashboardLayout.jsx
@@ -1,3 +1,4 @@
+import { useLocation } from 'react-router-dom'
 import React from 'react'
 import { Link, NavLink, useNavigate } from 'react-router-dom'
 import Button from '../components/Button'
@@ -8,25 +9,26 @@ const DashboardLayout = ({ children }) => {
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const location = useLocation();
     const { user } = useSelector(state => state.auth)
 
     return (
         <section className=''>
             <div className='flex py-3 px-3 justify-between items-center gap-y-5  my-3 text-lg bg-slate-900 rounded-lg border border-slate-600'>
                 <span className='space-x-1 md:space-x-3'>
-                    <NavLink to={"/dashboard"} className='hover:bg-slate-700 transition-all duration-300 px-3 py-1 rounded-full'>
+                    <NavLink to={"/dashboard"} className={`hover:bg-slate-700 transition-all duration-300 px-3 py-1 rounded-full ${location.pathname === "/dashboard" && "bg-slate-600"}`}>
                         Profile
                     </NavLink>
                     {
                         user.role === "admin" ? <>
-                            <Link to={"/dashboard/create-quiz"} className='hover:bg-slate-700 transition-all duration-300 px-3 py-1 rounded-full'>
+                            <Link to={"/dashboard/create-quiz"} className={`hover:bg-slate-700 transition-all duration-300 px-3 py-1 rounded-full ${location.pathname.includes("create") && "bg-slate-600"}`}>
                                 Create
                             </Link>
-                            <Link to={"/dashboard/quizes"} className='hover:bg-slate-700 transition-all duration-300 px-3 py-1 rounded-full'>
+                            <Link to={"/dashboard/quizes"} className={`hover:bg-slate-700 transition-all duration-300 px-3 py-1 rounded-full ${location.pathname.includes("quizes") && "bg-slate-600"}`}>
                                 Quizes
                             </Link>
                         </> : <>
-                            <Link to={"/dashboard/history"} className='hover:bg-slate-700 transition-all duration-300 px-3 py-1 rounded-full'>
+                            <Link to={"/dashboard/history"} className={`hover:bg-slate-700 transition-all duration-300 px-3 py-1 rounded-full ${location.pathname.includes("history") && "bg-slate-600"}`}>
                                 History
                             </Link>
 
